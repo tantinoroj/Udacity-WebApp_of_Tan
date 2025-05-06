e1c63e6dc6528a703eb839896f18ec4ed48502a7
tantinoroj <tan.t@chevron.com>
2025-05-06 12:28:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
-import logging
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '2971343e-c5d9-4bce-a664-56e7d292b42e'
+    SECRET_KEY = '2971343e-c5d9-4bce-a664-56e7d292b42e'
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'image30'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '6g1dmgSmM4xJuujBArXCDZnGUe4kmxavKi2qyCc8qJ3RseIHhZRBLg/9xcclMiLY+oustPrFyXE/+AStKi8CwQ=='
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'image'
-    
     BLOB_CONNECTION_STRING = os.environ.get('BLOB_CONNECTION_STRING') or 'DefaultEndpointsProtocol=https;AccountName=image30;AccountKey=6g1dmgSmM4xJuujBArXCDZnGUe4kmxavKi2qyCc8qJ3RseIHhZRBLg/9xcclMiLY+oustPrFyXE/+AStKi8CwQ==;EndpointSuffix=core.windows.net'
     if not BLOB_CONNECTION_STRING:
         logger.error("BLOB_CONNECTION_STRING is not set")
@@ -35,7 +31,7 @@ class Config(object):
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
     CLIENT_SECRET  = '5038Q~N~2KCnaRV3gov~STzRepw6hcINpKXh4b9z'
-    AUTHORITY = "https://login.microsoftonline.com/ff873fe8-6631-416d-9262-bdbd56117dae"
+    AUTHORITY = 'https://login.microsoftonline.com/ff873fe8-6631-416d-9262-bdbd56117dae'
     CLIENT_ID = '416d6f76-eb16-48ac-a864-59b8326309a1'
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
